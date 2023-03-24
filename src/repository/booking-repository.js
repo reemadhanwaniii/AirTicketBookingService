44da9354275811d639eed5abe1e4de90d6aecd45
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-24 19:30:07 +0530

    Added create booking repository code

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,19 @@
+const { Booking }  = require('../models/index');
+const { StatusCodes } = require('http-status-codes');
+const { ValidationError,AppError } = require('../utils/errors/index');
+
+class BookingRepository {
+    async create(data) {
+        try {
+            const booking = await Booking.create(data);
+            return booking;
+        }catch(error) {
+            if(error.name == 'SequelizeValidationError') {
+                throw new ValidationError(error);
+            }
+            throw new AppError('RepositoryError',
+            'There was some issue creating the booking,please try again later',
+            StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
