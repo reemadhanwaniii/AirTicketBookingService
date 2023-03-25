48a908ff6fb0ff7240c151792fef5ecbd253ee3f
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-25 18:29:37 +0530

    Completed create booking

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { BookingController } = require('../../controllers/index');
+
+
+router.post('/booking',BookingController.create);
 
 module.exports = router;
