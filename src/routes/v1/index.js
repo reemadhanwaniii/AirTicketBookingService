84e7d743aaad2bf2dd4f570af9fc3b6dc35b3d48
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-30 19:04:31 +0530

    Added rabbit mq config

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { BookingController } = require('../../controllers/index');
+const bookingController = new BookingController();
 
+router.post('/booking',bookingController.create);
 
-router.post('/booking',BookingController.create);
+router.post('/publish',bookingController.sendMessageToQueue);
 
 module.exports = router;
